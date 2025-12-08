0637201ef626bd0685ceb71c69d1fd71b1214196
Artillepsy <artillepsy@gmail.com>
2025-12-08 13:07:11 +0100

    feat: customized fields in the config and its model

--- JobSearch.DataScraper/Src/Services/Configuration/Scrapers/CareersInPolandConfig.cs
+++ JobSearch.DataScraper/Src/Services/Configuration/Scrapers/CareersInPolandConfig.cs
@@ -2,11 +2,16 @@ namespace JobSearch.DataScraper.Services.Configuration.Scrapers;
 
 public class CareersInPolandConfig
 {
-	//todo: max retries
-	public string Url { get; set; } = "";
-	public float PingIntervalSecMin { get; set; }
-	public float PingIntervalSecMax { get; set; }
+	public string BaseUrl { get; set; } = "";
+	public int PageStartIndex { get; set; }
+	public float SearchIntervalMin { get; set; }
+	public float SearchIntervalMax { get; set; }
+	public int MaxRetries { get; set; }
 
 	public override string ToString() =>
-		$"url: {Url}, ping interval min: {PingIntervalSecMin}, ping interval max: {PingIntervalSecMax}";
+		$"BaseUrl: {BaseUrl}\n" +
+		$"PageStartIndex: {PageStartIndex}\n" +
+		$"SearchIntervalMin: {SearchIntervalMin}\n" +
+		$"SearchIntervalMin: {SearchIntervalMax}\n" +
+		$"MaxRetries: {MaxRetries}\n";
 }
