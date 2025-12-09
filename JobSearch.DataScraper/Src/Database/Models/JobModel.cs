5f0d7501de879776821bbb616af28f9bdd906cab
Artillepsy <artillepsy@gmail.com>
2025-12-09 22:23:40 +0100

    refactor: change job model fields' structure

--- JobSearch.DataScraper/Src/Database/Models/JobModel.cs
+++ JobSearch.DataScraper/Src/Database/Models/JobModel.cs
@@ -9,10 +9,11 @@ public class JobModel
 	public string? Location { get; set; }
 
 	public bool IsSalaryVisible { get; set; }
-	public decimal? SalaryFrom { get; set; }
-	public decimal? SalaryTo { get; set; }
+	public string Salary { get; set; } = "";
 
-	public string SourceWebsite { get; set; } = "";
+	public string Website { get; set; } = "";
+	public string WebsiteSpecificId { get; set; } = "";
+	
 	public string Url { get; set; } = "";
 	public DateTime CreatedAt { get; set; }
 }
