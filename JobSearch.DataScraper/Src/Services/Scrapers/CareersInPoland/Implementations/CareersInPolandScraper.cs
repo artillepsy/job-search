a052585fde5f9cf1a33f2ba7ed63ab22b23e6178
Artillepsy <artillepsy@gmail.com>
2025-12-11 17:15:22 +0100

    feat: move repeatable code into a base class, make regions, add repository workflow

--- JobSearch.DataScraper/Src/Services/Scrapers/CareersInPoland/Implementations/CareersInPolandScraper.cs
+++ JobSearch.DataScraper/Src/Services/Scrapers/CareersInPoland/Implementations/CareersInPolandScraper.cs
@@ -1,6 +1,9 @@
 using System.Text.Json;
 using JobSearch.DataScraper.Core.Random;
+using JobSearch.DataScraper.Core.Urls;
 using JobSearch.DataScraper.Core.Utils;
+using JobSearch.DataScraper.Database.Models;
+using JobSearch.DataScraper.Database.Repositories;
 using JobSearch.DataScraper.Services.Configuration.Scrapers;
 using JobSearch.DataScraper.Services.Options;
 using JobSearch.DataScraper.Services.Result;
@@ -10,24 +13,26 @@ using Microsoft.Extensions.Options;
 
 namespace JobSearch.DataScraper.Services.Scrapers.CareersInPoland.Implementations;
 
+//todo: add chunks of data with higher load, but since there are tens of pages only, optimization can wait
 public class CareersInPolandScraper : ScraperBase
 {
 	private readonly IRandomService _randomService;
-	private readonly HttpClient _httpClient;
 	private readonly CareersInPolandConfig _config;
-	private bool _isRunning = false;
 
 	public CareersInPolandScraper(
-		IRandomService randomService,
 		ILogger<CareersInPolandScraper> logger, 
 		IHttpClientFactory httpClientFactory, 
-		IOptions<CareersInPolandConfig> options) : base(logger, httpClientFactory)
+		IUrlHashService urlHashService,
+		IJobRepository repository,
+		IRandomService randomService,
+		IOptions<CareersInPolandConfig> options) : base(logger, httpClientFactory, urlHashService, repository)
 	{
 		_randomService = randomService;
 		_config = options.Value;
-		_httpClient = httpClientFactory.CreateClient();
 		_logger.LogInformation($"[constructor] config: {_config}");
 	}
+
+	#region Scraping
 	
 	public override bool IsRunning() => _isRunning;
 
@@ -35,83 +40,118 @@ public class CareersInPolandScraper : ScraperBase
 	public override async Task<ScrapingResult> ScrapeAsync(IScrapingOptions options, CancellationToken ct)
 	{
 		_isRunning = true;
-		var currPage = _config.PageStartIndex;
-		var pageModels = new List<CareersInPolandPageModel>();
-		
-		var result = new ScrapingResult()
+		var result = new ScrapingResult() { StartedAt = DateTime.UtcNow };
+
+		try
 		{
-			StartedAt = DateTime.UtcNow
-		};
-		
-		while (!ct.IsCancellationRequested)
-		{
-			try
-			{
-				var pageModel = await GetPageAsync(currPage, ct);
-				pageModels.Add(pageModel);
-				
-				_logger.LogInformation(
-					$"page {currPage} " +
-					$"of size {MemoryHelper.GetSerializedSize(pageModel)} B received. " +
-					$"Total size: {MemoryHelper.GetSerializedSize(pageModels)} B");
-				
-				result.RecordsScraped += pageModel.JobOffers.Pagination.Data.Count;
-				
-				if (pageModel.JobOffers.Pagination.IsLastPage)
-				{
-					break;
-				}
-			}
-			catch (Exception e)
-			{
-				result.IsSuccess = false;
-				result.Error = e.Message;
+			var pageModels = await GetAllPagesAsync(ct);
+			result.RecordsScraped = pageModels.Sum(m => m.JobOffers.Pagination.Data.Count);
+			result.IsSuccess = true;
 			
-				_logger.LogError(e, message: "scraping failed");
-			}
+			
+			_logger.LogInformation( $"Scraping finished");
+		}
+		catch (Exception e)
+		{
+			result.IsSuccess = false;
+			result.Error = e.Message;
 
-			currPage++;
-				
-			int randomDelay = _randomService.NextInt(_config.SearchIntervalMin, _config.SearchIntervalMax);
-			_logger.LogInformation($"waiting for  {randomDelay} millisec");
-
-			await Task.Delay(randomDelay, ct);
+			_logger.LogError(e, message: "Scraping failed");
 		}
 
-		result.IsSuccess = true;
 		result.FinishedAt = DateTime.UtcNow;
 		result.Duration = result.FinishedAt.Value - result.StartedAt;
 		
-		_logger.LogInformation( $"scraping finished. Result: {result}");
+		_logger.LogInformation( $"Scraping result: {result}");
 
 		_isRunning = false;
 		return result;
 	}
 
+	#endregion
+
+	#region Models
+
+	private async Task CacheJobModels(IEnumerable<CareersInPolandPageModel> pageModels)
+	{
+		var jobModels = new List<JobModel>();
+
+		foreach (var page in pageModels)
+		foreach (var data in page.JobOffers.Pagination.Data)
+		foreach (var location in data.Locations)
+		{
+			var jobModel = new JobModel()
+			{
+				CompanyName = data.EmployerName,
+				CreatedAt = data.Date,
+				Website = _config.Website,
+				WebsiteSpecificId = data.Id,
+				IsSalaryVisible = !string.IsNullOrEmpty(data.Salary),
+				Salary = data.Salary,
+				Location = location.Location,
+				Title = data.Title,
+				Url = location.FullUrl,
+				Sha1UrlHash = _urlHashService.HashUrl(data.Id, location.FullUrl),
+			};
+			jobModels.Add(jobModel);
+		}
+
+		await _repository.AddUniqueAsync(jobModels);
+	}
+
+	#endregion
+	
+	#region Pagination
+
+	private async Task<List<CareersInPolandPageModel>> GetAllPagesAsync(CancellationToken ct)
+	{
+		var pageModels = new List<CareersInPolandPageModel>();
+		var currPage = _config.PageStartIndex;
+
+		while (!ct.IsCancellationRequested)
+		{
+			var pageModel = await GetPageAsync(currPage, ct);
+			pageModels.Add(pageModel);
+
+			_logger.LogInformation(
+				$"[GetAllPagesAsync] page {currPage} " +
+				$"of size {MemoryHelper.GetSerializedSize(pageModel)} B received. " +
+				$"Total size: {MemoryHelper.GetSerializedSize(pageModels)} B");
+
+			if (pageModel.JobOffers.Pagination.IsLastPage)
+			{
+				break;
+			}
+
+			currPage++;
+
+			int randomDelay = _randomService.NextInt(_config.SearchIntervalMin, _config.SearchIntervalMax);
+			_logger.LogInformation($"waiting for  {randomDelay} millisec");
+
+			await Task.Delay(randomDelay, ct);
+		}
+		
+		return pageModels;
+	}
+
 	private async Task<CareersInPolandPageModel> GetPageAsync(int page, CancellationToken ct)
 	{
-		
 		var url = $"{_config.BaseUrl}{page}";
 		CareersInPolandPageModel model;
-		
-		try
-		{
-			/*var response = await _httpClient.GetAsync(url, ct);
-			response.EnsureSuccessStatusCode();
-			
-			var jsonString = await response.Content.ReadAsStringAsync(ct);*/
 
-			var jsonString = CareersInPolandDebug.GetMockJsonPage(page);
-			model = JsonSerializer.Deserialize<CareersInPolandPageModel>(jsonString) ?? throw new InvalidOperationException();
-			
-			//_logger.LogInformation($"json model: {model}\nsize: {MemoryHelper.GetSerializedSize(model)}");
-		}
-		catch (Exception e)
-		{
-			_logger.LogError(e, "page retrieving failed");
-			throw;
-		}
+		/*var response = await _httpClient.GetAsync(url, ct);
+		response.EnsureSuccessStatusCode();
+
+		var jsonString = await response.Content.ReadAsStringAsync(ct);*/
+
+		var jsonString = CareersInPolandDebug.GetMockJsonPage(page);
+		model = JsonSerializer.Deserialize<CareersInPolandPageModel>(jsonString) ??
+		        throw new InvalidOperationException();
+
+		//_logger.LogInformation($"json model: {model}\nsize: {MemoryHelper.GetSerializedSize(model)}");
 
 		return model;
 	}
+
+	#endregion
 }
