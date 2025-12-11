f48f14ff694837519bce1c941fbe7b9cd7657040
Artillepsy <artillepsy@gmail.com>
2025-12-11 17:14:42 +0100

    feat: add website field for config

--- JobSearch.DataScraper/Src/Services/Configuration/Scrapers/CareersInPolandConfig.cs
+++ JobSearch.DataScraper/Src/Services/Configuration/Scrapers/CareersInPolandConfig.cs
@@ -2,6 +2,7 @@ namespace JobSearch.DataScraper.Services.Configuration.Scrapers;
 
 public class CareersInPolandConfig
 {
+	public string Website { get; set; } = "";
 	public string BaseUrl { get; set; } = "";
 	public int PageStartIndex { get; set; }
 	public int SearchIntervalMin { get; set; } // millisec
@@ -9,6 +10,7 @@ public class CareersInPolandConfig
 	public int MaxRetries { get; set; }
 
 	public override string ToString() =>
+		$"Website: {Website}\n" +
 		$"BaseUrl: {BaseUrl}\n" +
 		$"PageStartIndex: {PageStartIndex}\n" +
 		$"SearchIntervalMin: {SearchIntervalMin}\n" +
