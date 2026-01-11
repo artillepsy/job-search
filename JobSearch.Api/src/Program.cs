cd2485d80c9053328bfdea6684ef6e4f670b3981
Artillepsy <artillepsy@gmail.com>
2026-01-12 00:25:16 +0100

    refactor: enable Swagger UI in all environments and remove unused development-only configurations

--- JobSearch.Api/src/Program.cs
+++ JobSearch.Api/src/Program.cs
@@ -24,53 +24,20 @@ builder.Services.AddSingleton<ITokenService, DevTokenService>();
 
 var app = builder.Build();
 
+// Add Swagger UI to test it in Production
+app.MapOpenApi();
+app.UseSwaggerUI(o =>
+{
+	o.SwaggerEndpoint("/openapi/v1.json", "API v1");
+	o.RoutePrefix = "swagger"; 
+});
+
 if (app.Environment.IsDevelopment())
 {
 	app.UseHttpsRedirection();
 }
 
-// swagger + openApi
-if (app.Environment.IsDevelopment())
-{
-	app.MapOpenApi();
-	app.UseSwaggerUI(o =>
-	{
-		o.SwaggerEndpoint("/openapi/v1.json", "API v1");
-		o.RoutePrefix = "swagger"; // UI at /swagger
-	});
-}
-
 // map api endpoints
 app.MapControllers();
-
-// spa proxy, exclude swagger, api and openapi
-app.MapWhen(ctx =>
-		!ctx.Request.Path.StartsWithSegments("/api", StringComparison.OrdinalIgnoreCase) &&
-		!ctx.Request.Path.StartsWithSegments("/swagger", StringComparison.OrdinalIgnoreCase) &&
-		!ctx.Request.Path.StartsWithSegments("/openapi", StringComparison.OrdinalIgnoreCase),
-	spaApp =>
-	{
-		spaApp.UseSpa(spa =>
-		{
-			if (app.Environment.IsDevelopment())
-				spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
-		});
-	});
-
-// Migrate DB (dev only or guarded)
-if (app.Environment.IsDevelopment())
-{
-	using var scope = app.Services.CreateScope();
-	var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-	
-	try
-	{
-		await db.Database.MigrateAsync();
-	}
-	catch (Exception ex)
-	{
-		app.Logger.LogError(ex, "Database migration failed");
-	}
-}
 
 app.Run();
