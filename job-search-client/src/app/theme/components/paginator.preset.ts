f28d4542613efa343bf46e2fa76a60b017542cf8
Artillepsy <artillepsy@gmail.com>
2026-01-23 12:00:37 +0100

    refactor: update job-item and job-items-board styles for improved layout, spacing, and responsive design

--- job-search-client/src/app/theme/components/paginator.preset.ts
+++ job-search-client/src/app/theme/components/paginator.preset.ts
@@ -1,11 +1,11 @@
 export const PaginatorPreset = {
   root: {
-    gap: '0',
+    gap: '0.1rem',
     background: 'transparent',
   },
   navButton: {
-    hoverBackground: 'rgba(0,0,0,0.1)', // Darker gray hover
-    selectedBackground: 'rgba(0,0,0,0.37)', // Darker gray hover
+    hoverBackground: 'rgba(164,170,192,0.38)', // Darker gray hover
+    selectedBackground: 'rgb(164,170,192)', // Darker gray hover
     borderRadius: '50%',
     selectedColor: '#ffffff',
   },
