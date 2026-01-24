84036e19836a98b7411be50effb2244b5b5af92c
Artillepsy <artillepsy@gmail.com>
2026-01-24 11:24:45 +0100

    feat: implement USAJobs scraper data models

---
+++ JobSearch.DataScraper/src/Scraping/Scrapers/Implementations/UsaJobs/UsaJobsPageModel.cs
@@ -0,0 +1,78 @@
+namespace JobSearch.DataScraper.Scraping.Scrapers.Implementations.UsaJobs;
+
+public class UsaJobsPageModel
+{
+	public SearchResult SearchResult { get; set; }
+
+	public override string ToString()
+	{
+		return $"SearchResult: {SearchResult}";
+	}
+}
+
+public class SearchResult
+{
+	public int SearchResultCount { get; set; }
+	public int SearchResultCountAll { get; set; }
+	public UserArea UserArea { get; set; }
+	public List<SearchResultItem> SearchResultItems { get; set; }
+	
+	public override string ToString()
+	{
+		return
+			$"Number of pages: {UserArea.NumberOfPages}\n" +
+			$"Count: {SearchResultCount}\n" +
+			$"Total Found: {SearchResultCountAll}\n" +
+			string.Join("\n", SearchResultItems.Select((item, index) => $"[{index}] {item}")) + "\n";
+	}
+}
+
+public class UserArea
+{
+	public int NumberOfPages { get; set; }
+	
+}
+
+public class SearchResultItem
+{
+	public MatchedObjectDescriptor MatchedObjectDescriptor { get; set; }
+
+	public override string ToString()
+	{
+		return MatchedObjectDescriptor?.ToString() ?? "No Descriptor Available";
+	}
+}
+
+public class MatchedObjectDescriptor
+{
+	public string PositionTitle { get; set; }
+	public string PositionURI { get; set; }
+	public string PositionLocationDisplay { get; set; }
+	public string OrganizationName { get; set; }
+	public bool RemoteIndicator { get; set; }
+	public DateTime PublicationStartDate { get; set; }
+	public PositionRemuneration PositionRemuneration { get; set; }
+
+	public override string ToString()
+	{
+		return
+			$"- Title: {PositionTitle}\n" +
+			$"- Org: {OrganizationName}\n" +
+			$"- Remote: {RemoteIndicator}\n" +
+			$"- Location: {PositionLocationDisplay}\n" +
+			$"- Date: {PublicationStartDate:yyyy-MM-dd}\n" +
+			$"- Salary: {PositionRemuneration}\n" +
+			$"- URL: {PositionURI}";
+	}
+}
+
+public class PositionRemuneration
+{
+	public int MinimumRange { get; set; }
+	public int MaximumRange { get; set; }
+
+	public override string ToString()
+	{
+		return $"{MinimumRange} - {MaximumRange}";
+	}
+}
