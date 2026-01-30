d34cee74a892f45c22804e87ce78d96b4ab317b5
Artillepsy <artillepsy@gmail.com>
2026-01-30 22:14:30 +0100

    refactor: replace FiltersDropdownComponent with FiltersComponent for improved simplicity and maintainability

--- job-search-client/src/app/features/jobs/components/filters/filters-dropdown.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-filters-dropdown',
-  imports: [],
-  templateUrl: './filters-dropdown.component.html',
-  styleUrl: './filters-dropdown.component.scss',
-})
-export class FiltersDropdownComponent {}
